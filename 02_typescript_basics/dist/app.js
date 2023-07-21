eb2dfefcdde6bc4ef7c96b7700c5491351991cb4
neighborpil <neighborpil@naver.com>
2023-07-21 13:34:32 +0900

    save

--- 02_typescript_basics/dist/app.js
+++ 02_typescript_basics/dist/app.js
@@ -1,6 +1,16 @@
 "use strict";
+let appId = 'abc';
 const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!!!');
-});
+function add(n1, n2) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
+function clickHandler(message, age) {
+    console.log('Clicked!' + message + age);
+}
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, 'Your welcome!', 30));
+}
 //# sourceMappingURL=app.js.map
--- 02_typescript_basics/src/app.ts
+++ 02_typescript_basics/src/app.ts
@@ -1,9 +1,18 @@
-const button = document.querySelector('button')!;
+let appId = 'abc';
+const button = document.querySelector('button')!; // ! means that we are sure that this element exists
 
-function clickHandler(message: string) {
-    console.log('Clicked!' + message);
+function add(n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
+
+function clickHandler(message: string, age: number) {
+    // let userName = 'Max';
+    console.log('Clicked!' + message + age);
 }
 // a comment
 if (button) {
-    button.addEventListener('click', clickHandler.bind(null, 'Your welcome!'));
+    button.addEventListener('click', clickHandler.bind(null, 'Your welcome!', 30));
 }
